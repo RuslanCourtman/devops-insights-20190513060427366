0c2f76fc9345e692893f9fd66f2fdb17fe3b818e
RuslanCourtman <50534389+RuslanCourtman@users.noreply.github.com>
2019-05-15 18:47:48 +1200

    Update api.specs.js

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -22,7 +22,7 @@
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?q=hamilton,nz'
+              url: appUrl + '/api/v1/getWeather?q=hamilton'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
@@ -60,7 +60,7 @@
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?q=hamilton,nz'
+              url: appUrl + '/api/v1/getWeather?q=hamilton'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
