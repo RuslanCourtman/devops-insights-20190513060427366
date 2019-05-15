3cb711f3a569629a9317a31246b15d37e5b3b41e
RuslanCourtman <50534389+RuslanCourtman@users.noreply.github.com>
2019-05-16 01:17:17 +1200

    Update controllers.js

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,3 +1,12 @@
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
 
 var ConsoleModule = angular.module('ConsoleModule', ['ngRoute']);
 
@@ -48,32 +57,7 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                     $scope.zip4City = response.data.city;
                     $scope.zip4Weather = response.data.weather;
                 } 
-                function initMap() {
-                  var myLatlng = {lat: -25.363, lng: 131.044};
-
-                  var map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 4,
-                    center: myLatlng
-                  });
-
-                  var marker = new google.maps.Marker({
-                    position: myLatlng,
-                    map: map,
-                    title: 'Click to zoom'
-                  });
-
-                  map.addListener('center_changed', function() {
-                    // 3 seconds after the center of the map has changed, pan back to the
-                    // marker.
-                    window.setTimeout(function() {
-                      map.panTo(marker.getPosition());
-                    }, 3000);
-                  });
-
-                  marker.addListener('click', function() {
-                    map.setZoom(8);
-                    map.setCenter(marker.getPosition());
-                  });
+                
                 }
             });
         } else {
