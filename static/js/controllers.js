4138147d64d5dd3483a67d9f2ed7ad69f33e47bd
RuslanCourtman <50534389+RuslanCourtman@users.noreply.github.com>
2019-05-16 00:23:53 +1200

    Update controllers.js

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -48,6 +48,33 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                     $scope.zip4City = response.data.city;
                     $scope.zip4Weather = response.data.weather;
                 } 
+                function initMap() {
+                  var myLatlng = {lat: -25.363, lng: 131.044};
+
+                  var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 4,
+                    center: myLatlng
+                  });
+
+                  var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: 'Click to zoom'
+                  });
+
+                  map.addListener('center_changed', function() {
+                    // 3 seconds after the center of the map has changed, pan back to the
+                    // marker.
+                    window.setTimeout(function() {
+                      map.panTo(marker.getPosition());
+                    }, 3000);
+                  });
+
+                  marker.addListener('click', function() {
+                    map.setZoom(8);
+                    map.setCenter(marker.getPosition());
+                  });
+                }
             });
         } else {
             if(which === 1) {
