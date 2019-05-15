80723a5e0c2aa0c7e76c89d018c3c809b5ecbbff
RuslanCourtman <50534389+RuslanCourtman@users.noreply.github.com>
2019-05-15 19:58:51 +1200

    Update api.specs.js

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -29,7 +29,7 @@
           	} else {
               assert.equal(resp.statusCode, 200);
               var pbody = JSON.parse(body);
-              assert((pbody.country === 'NZ'), "City name does not match");
+              assert((pbody.city === 'hamilton'), "City name does not match");
               done();
             }
         });
@@ -67,7 +67,7 @@
           	} else {
               assert.equal(resp.statusCode, 200);
               var pbody = JSON.parse(body);
-              assert(pbody.country === 'NZ', "City name does not match");
+              assert(pbody.city === 'hamilton', "City name does not match");
               done();
             }
         });
