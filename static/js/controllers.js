3a149fd9ef97cc690b30b10bb7334118c67b8479
RuslanCourtman <50534389+RuslanCourtman@users.noreply.github.com>
2019-05-15 12:55:00 +1200

    COMPX341-19 A3 VERSION 1
    Fully Working

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -16,7 +16,7 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     $scope.somemessage = "Some weather";
     $scope.zip1City = "";
     $scope.zip1Weather = "";
-
+        
     $scope.zip = function(which) {
 
         var data = "";
