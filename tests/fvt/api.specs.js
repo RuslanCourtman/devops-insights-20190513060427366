a56c7b7d6eda298ae4fc29b7cf237ccaad1dbe18
RuslanCourtman <50534389+RuslanCourtman@users.noreply.github.com>
2019-05-15 21:38:20 +1200

    Update api.specs.js

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -35,7 +35,7 @@
         });
     	});
 
-      it('without city name', function(done) {
+      it('without zip code', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
@@ -53,7 +53,7 @@
         });
     	});
 
-      it('with another valid city name', function(done) {
+      it('with another valid zip code', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
