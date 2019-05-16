74509d31bb50ab232e42f2adadf0a4f819c849fa
RuslanCourtman <50534389+RuslanCourtman@users.noreply.github.com>
2019-05-16 19:54:54 +1200

    Update controllers.js

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -61,8 +61,8 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                     $scope.zip1Weather = response.data.weather;
                         var coord1 =  response.data.coord;
                         
-                      var mySubLat = coord1.substring( 19,24 );
-                       var mySubLng = coord1.substring(7,13);
+                      var mySubLat = coord1.slice( 19,24 );
+                       var mySubLng = coord1.slice(7,13);
                         var markL1 = {lat: mySubLat, lng: mySubLng};
                         
                         
